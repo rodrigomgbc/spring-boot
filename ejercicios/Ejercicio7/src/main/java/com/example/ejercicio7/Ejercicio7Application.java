a1100b9b481fa1e4d9d8caeef88e26bcc5dd5b65
Rodrigo M <rodrigomgbc@gmail.com>
2026-04-09 09:36:02 +0200

    Creación del ejercicio 7

---
+++ ejercicios/Ejercicio7/src/main/java/com/example/ejercicio7/Ejercicio7Application.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ejercicio7Application.class, args);
+    }
+
+}
---
+++ ejercicios/Ejercicio7/src/test/java/com/example/ejercicio7/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
